58ce3c1ab4701311311beb59b2438b1d994723dc
Rodrigo Gunisalvo <gunisalvo@Rodrigos-MacBook-Air.local>
2014-10-31 23:14:11 -0200

    upgrade

--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,11 @@
 Vagrant::Config.run do |config|
+  config.ssh.username = "vagrant"
+  config.ssh.password = "vagrant"
+  config.ssh.timeout = 300
   config.vm.define :ubuntuJava do |ubuntuJava|
-    ubuntuJava.vm.box = "hashicorp/precise64"
+    ubuntuJava.vm.box = "ubuntu/trusty64"
+    ubuntuJava.vm.network :forward_port, host: 8080
+    ubuntuJava.gui = true
     ubuntuJava.vm.customize [ "modifyvm", :id, "--memory", "1024"]
     ubuntuJava.vm.provision :puppet do |puppet|
       puppet.module_path = "modules"
--- buildEnviroment.sh
+++ buildEnviroment.sh
@@ -1,11 +1,31 @@
  #!/bin/bash
- wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b13/jdk-7u71-linux-x64.tar.gz
- mkdir modules/java/files/
- mv jdk-7u71-linux-x64.tar.gz modules/java/files/
- puppet module install --modulepath modules puppetlabs-mongodb
- puppet module install --modulepath modules maestrodev/maven
- puppet module install --modulepath modules willdurand-nodejs
- vagrant up
- vagrant provision
- echo 'done! type: vagrant ssh'
- #end
+jdk=./modules/java/files/jdk-7u71-linux-x64.tar.gz
+if [ ! -e "$jdk" ]; then
+    echo "downloading JDK"
+    wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u71-b13/jdk-7u71-linux-x64.tar.gz
+ 	mkdir modules/java/files/
+	mv jdk-7u71-linux-x64.tar.gz modules/java/files/
+fi 
+
+mongodb=./modules/mongodb
+if [ ! -e "$mongodb" ]; then
+    echo "puppet formula download mongodb"
+ 	puppet module install --modulepath modules puppetlabs-mongodb
+fi
+
+maven=./modules/maven
+if [ ! -e "$maven" ]; then
+    echo "puppet formula download maven"
+	puppet module install --modulepath modules maestrodev/maven
+fi
+
+nodejs=./modules/nodejs
+if [ ! -e "$nodejs" ]; then
+    echo "puppet formula download nodejs"
+	puppet module install --modulepath modules willdurand-nodejs
+fi
+
+vagrant up
+vagrant provision
+echo "done! type: vagrant ssh"
+#end
